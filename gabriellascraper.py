5c2181798e6c5447c7b4d229bb21733ce970f422
Stephanie Li <stephanieeeli8@berkeley.edu>
2020-05-21 14:53:29 -0700

    Refactor scraper

--- gabriellascraper.py
+++ gabriellascraper.py
@@ -1,70 +1,64 @@
 #!/usr/bin/python
-import bs4, requests, smtplib
+"""PlantScraper v1.0
+Scrape GabriellaPlants.com and Logees.com for rare plants!
+First update the configuration file to include email secrets,
+and include any additional plants you wish to track. Then,
+run this script in a cron job and wait! We recommend a delay
+of ~30sec between runs, although neither site explicitly states
+a preference (no robot.txt).
+"""
 
+import bs4
+import requests
+import smtplib
+
+# TODO: move these constants into config file
+# Format: (short name, full name, url)
+plant_templates = [
+    ('PPP', '4" philodendron pink princess', 'https://www.gabriellaplants.com/collections/home-page/products/4-pink-princess-philodendron'),
+    ('5" Jose Buono', '5" philodendron jose buono', 'https://www.gabriellaplants.com/collections/home-page/products/5-philodendron-jose-buono'),
+    ('4" Jose Buono', '5" philodendron jose buono', 'https://www.gabriellaplants.com/collections/home-page/products/4-jose-bueno-philodendron'),
+    ('4 Albo Syngonium', '4" syngonium podophyllum albo-variegatum', 'https://www.gabriellaplants.com/collections/home-page/products/4-variegated-emerald-gem-syngonium-arrow-head-house-plant-nephthytis'),
+    ('3" Gabby', '3" philodendron \'gabby\' sport', 'https://www.gabriellaplants.com/collections/home-page/products/3-gabby-philodendron-sport'),
+]
+# Email addresses to send to
 toAddress = []
-getPage = requests.get('https://www.gabriellaplants.com/collections/home-page?sort_by=price-descending')
-getPage.raise_for_status()
+# My email secrets
+my_email = ""
+my_password = ""
 
-inventory = bs4.BeautifulSoup(getPage.text, 'html.parser')
-plants = inventory.select('.cd.chp')
+def main():
+    # If there are in-stock matches, append their template to this list
+    matched = []
 
-plant_availability = False
+    # Scrape page of Gabriella for plants
+    getPage = requests.get('https://www.gabriellaplants.com/collections/home-page?sort_by=price-descending')
+    getPage.raise_for_status()
+    inventory = bs4.BeautifulSoup(getPage.text, 'html.parser')
+    for plant in inventory.select('.cd.chp'):  # Iterate through all plants on the website
+        # Attempt to match against all templates' full names
+        for j, (_, full_name, _) in enumerate(plant_templates):
+            # Slide a window across the website text to match for these full plant names as a substring
+            for i in range(len(plant.text)):
+                chunk = plant.text[i:i+len(full_name)].lower()
+                if chunk == full_name:
+                    matched.append(plant_templates[j])
 
-ppp = '4" philodendron pink princess'
-ppp_stock = False 
+    if matched:
+        # Send to Gmail
+        conn = smtplib.SMTP('smtp.gmail.com', 587)
+        conn.ehlo()
+        conn.starttls()
+        conn.login(my_email, my_password)
+        for short_name, _, url in matched_templates:
+            conn.sendmail(my_email, toAddress,
+                          ('Subject: %s In Stock Alert\n\nCome get your '
+                           '%s from Gabriella\'s!\n\n%s\n\n'
+                           'Plant Notifier V1.0') %s (short_name, short_name, url))
+        conn.quit()
+        print('Sent notifs')
+    else:
+        print('Out of stock')
 
-jose5 = '5" philodendron jose buono'
-jose5_stock = False
-
-jose4 = '4" philodendron jose buono'
-jose4_stock = False 
-
-syng4 = '4" syngonium podophyllum albo-variegatum'
-syng4_stock = False
-
-gabby3 = '3" philodendron \'gabby\' sport'
-gabby3_stock = False
-
-wantedplants = [ppp, jose5, jose4, syng4, gabby3]
-
-for plant in plants:
-    for want in wantedplants:
-        flength = len(want)
-        for i in range(len(plant.text)):
-            chunk = plant.text[i:i+flength].lower()
-            if chunk == ppp:
-                ppp_stock = True
-                plant_availability = True
-            if chunk == jose5:
-                jose5_stock = True
-                plant_availability = True
-            if chunk == jose4:
-                jose4_stock = True
-                plant_availability = True 
-            if chunk == syng4:
-                syng4_stock = True
-                plant_availability = True
-            if chunk == gabby3:
-                gabby3_stock = True
-                plant_availability = True
-
-if plant_availability == True:
-    conn = smtplib.SMTP('smtp.gmail.com', 587)
-    conn.ehlo()
-    conn.starttls()
-    conn.login('enter email here', 'enter email password')
-    if ppp_stock == True:
-        conn.sendmail('put email here', toAddress,'Subject: PPP In Stock Alert\n\nCome get your PPP from Gabriella\'s!\n\nhttps://www.gabriellaplants.com/collections/home-page/products/4-pink-princess-philodendron\n\nPlant Notifier V1.0')
-    if jose5_stock == True:
-        conn.sendmail('put email here', toAddress, 'Subject: 5" Jose Buono In Stock Alert\n\nCome get your 5" Jose Buono from Gabriella\'s!\n\nhttps://www.gabriellaplants.com/collections/home-page/products/5-philodendron-jose-buono\n\nPlant Notifier V1.0')
-    if jose4_stock == True:
-        conn.sendmail('put email here', toAddress, 'Subject: 4" Jose Buono In Stock Alert\n\nCome get your 4" Jose Buono from Gabriella\'s!\n\nhttps://www.gabriellaplants.com/collections/home-page/products/4-jose-bueno-philodendron\n\nPlant Notifier V1.0')
-    if syng4_stock == True:
-        conn.sendmail('put email here', toAddress, 'Subject: 4" Albo Syngonium In Stock Alert\n\nCome get your 4" Albo Syngonium from Gabriella\'s!\n\nhttps://www.gabriellaplants.com/collections/home-page/products/4-variegated-emerald-gem-syngonium-arrow-head-house-plant-nephthytis\n\nPlant Notifier V1.0')
-    if gabby3_stock == True:
-        conn.sendmail('put email here', toAddress, 'Subject: 3" Gabby In Stock Alert\n\nCome get your 3" Gabby from Gabriella\'s!\n\nhttps://www.gabriellaplants.com/collections/home-page/products/3-gabby-philodendron-sport\n\nPlant Notifier V1.0')
-    conn.quit()
-    print('Sent notifs')
-else:
-    print('Out of stock')
-
+if __name__ == "__main__":
+    main()
