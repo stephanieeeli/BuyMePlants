054caeea864dc4a287d7be0d19740654d70194cd
Stephanie Li <stephanieeeli8@berkeley.edu>
2020-05-21 14:32:32 -0700

    Working Gabriella scraper; before refactor

--- gabriellascraper.py
+++ gabriellascraper.py
@@ -11,53 +11,48 @@ plants = inventory.select('.cd.chp')
 plant_availability = False
 
 ppp = '4" philodendron pink princess'
-flength_ppp = len(ppp)
 ppp_stock = False 
 
 jose5 = '5" philodendron jose buono'
-flength_jose5 = len(jose5)
 jose5_stock = False
 
 jose4 = '4" philodendron jose buono'
-flength_jose4 = len(jose4)
 jose4_stock = False 
 
 syng4 = '4" syngonium podophyllum albo-variegatum'
-flength_syng4 = len(syng4)
 syng4_stock = False
 
 gabby3 = '3" philodendron \'gabby\' sport'
-flength_gabby3 = len(gabby3)
 gabby3_stock = False
 
+wantedplants = [ppp, jose5, jose4, syng4, gabby3]
+
 for plant in plants:
-    for i in range(len(plant.text)):
-        chunk_ppp = plant.text[i:i+flength_ppp].lower()
-        chunk_jose5 = plant.text[i:i+flength_jose5].lower()
-        chunk_jose4 = plant.text[i:i+flength_jose4].lower()
-        chunk_syng4 = plant.text[i:i+flength_syng4].lower()
-        chunk_gabby3 = plant.text[i:i+flength_gabby3].lower()
-        if chunk_ppp == ppp:
-            ppp_stock = True
-            plant_availability = True
-        if chunk_jose5 == jose5:
-            jose5_stock = True
-            plant_availability = True
-        if chunk_jose4 == jose4:
-            jose4_stock = True
-            plant_availability = True 
-        if chunk_syng4 == syng4:
-            syng4_stock = True
-            plant_availability = True
-        if chunk_gabby3 == gabby3:
-            gabby3_stock = True
-            plant_availability = True
+    for want in wantedplants:
+        flength = len(want)
+        for i in range(len(plant.text)):
+            chunk = plant.text[i:i+flength].lower()
+            if chunk == ppp:
+                ppp_stock = True
+                plant_availability = True
+            if chunk == jose5:
+                jose5_stock = True
+                plant_availability = True
+            if chunk == jose4:
+                jose4_stock = True
+                plant_availability = True 
+            if chunk == syng4:
+                syng4_stock = True
+                plant_availability = True
+            if chunk == gabby3:
+                gabby3_stock = True
+                plant_availability = True
 
 if plant_availability == True:
-    conn = smtplin.SMTP('smtp.gmail.com', 587)
+    conn = smtplib.SMTP('smtp.gmail.com', 587)
     conn.ehlo()
     conn.starttls()
-    conn.login('put email here', 'email password')
+    conn.login('enter email here', 'enter email password')
     if ppp_stock == True:
         conn.sendmail('put email here', toAddress,'Subject: PPP In Stock Alert\n\nCome get your PPP from Gabriella\'s!\n\nhttps://www.gabriellaplants.com/collections/home-page/products/4-pink-princess-philodendron\n\nPlant Notifier V1.0')
     if jose5_stock == True:
